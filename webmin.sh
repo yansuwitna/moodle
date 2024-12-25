9629632e9bc87a3d9382033d2ad31eabd00d3bc9
Yansuwitna <yansuwitna@gmail.com>
2024-12-25 23:06:37 +0800

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -6,7 +6,7 @@ apt update
 
 # Install dependencies
 echo "Installing dependencies..."
-sudo apt install -y software-properties-common apt-transport-https curl
+apt install -y software-properties-common apt-transport-https curl
 
 # Add Webmin repository GPG key
 echo "Adding Webmin GPG key..."
@@ -14,20 +14,20 @@ curl -fsSL https://packages.webmin.com/gpg.key | sudo tee /etc/apt/trusted.gpg.d
 
 # Add Webmin repository
 echo "Adding Webmin repository..."
-sudo add-apt-repository "deb http://packages.webmin.com/repository/webmin sarge contrib"
+add-apt-repository "deb http://packages.webmin.com/repository/webmin sarge contrib"
 
 # Update package index again after adding the repository
 echo "Updating package index..."
-sudo apt update
+apt update
 
 # Install Webmin
 echo "Installing Webmin..."
-sudo apt install -y webmin
+apt install -y webmin
 
 # Allow Webmin through the firewall (if UFW is active)
 if sudo ufw status | grep -q 'active'; then
   echo "Allowing Webmin through the firewall..."
-  sudo ufw allow 10000
+  ufw allow 10000
 fi
 
 # Display server IP address
